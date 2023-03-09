de9f3ae2cb18d00f8eb0b9fb20a6be0616f43f1e
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-09 16:36:30 +0800

    init

---
+++ 2-3-s2/express_basic/app.js
@@ -0,0 +1,18 @@
+// Include express from node_modules
+const express = require('express')
+const app = express()
+// Define server related variables
+const port = 3000
+
+
+
+// Handle request and response here
+app.get('/', (req, res) => {
+    res.send(`This is my first Express Web App`)
+  })
+
+
+
+
+
+
