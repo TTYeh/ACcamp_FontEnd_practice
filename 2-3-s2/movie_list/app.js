e1e8cab69953494349f2ec1276df3f062cf8ef49
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-14 16:16:09 +0800

    Add movieList dynamic loading

--- 2-3-s2/movie_list/app.js
+++ 2-3-s2/movie_list/app.js
@@ -5,14 +5,26 @@ const port = 3000
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
 // routes setting
-app.get('/', (req, res ) => {
-    res.render('index')
+app.get('/', (req, res) => {
+ 
+    // past the movie data into 'index' partial template
+    res.render('index', { movies: movieList.results })
+    
+  })
+
+  app.get('/movies/:movie_id', (req, res) => {
+    const movie = movieList.results.find(movie => movie.id.toString() === req.params.movie_id)
+    res.render('show', { movie: movie })
   })
 
 // start and listen on the Express server
