341410e9a499d440541fdfe03d9060b7de8e3aa0
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-14 14:44:26 +0800

    add movie list example

--- 2-3-s2/express_basic/app.js
+++ 2-3-s2/express_basic/app.js
@@ -7,10 +7,25 @@ const port = 3000
 
 
 // Handle request and response here
-app.get('/', (req, res) => {
-    res.send(`This is my first Express Web App`)
-  })
+app.get('/', (req, res ) => {
+  res.render('index')
+})
 
+  
+app.get('/food', (req, res) => {
+  res.send('My favorite food is ice cream')
+})
+
+// change into dynamic routes with params ':language'
+app.get('/popular/languages/:language', (req, res) => {
+  console.log(req)
+  res.send(`<h1>${req.params.language} is a popular language</h1>`)
+})
+
+// Start and listen the server
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
 
 
 
